e689a8328f940e8e764f3b474280ad3762eb83f6
techieadi4703 <techie.adi47@gmail.com>
2025-01-23 17:16:37 +0530

    imporved signup login page and added font

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,11 +1,9 @@
 import React, { useState } from "react";
-import GridLines from "react-gridlines";
 
-const SignupPage = () => {
+const LoginPage = () => {
   const [formData, setFormData] = useState({
-    fullname: "",
     username: "",
-    email: "",
+    privateKey: "",
   });
 
   const handleChange = (e) => {
@@ -14,78 +12,59 @@ const SignupPage = () => {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log("Form Data:", formData);
+    console.log("Login Data:", formData);
   };
 
   return (
     <>
-      {/* <GridLines
-        className="grid-area"
-        cellWidth={50}
-        strokeWidth={1}
-        cellWidth2={50}
-        > */}
-        
-        <div className="bg-[#f5f2e5] h-screen flex items-center justify-center">
-          <div className="border-2 border-black p-10  rounded-lg relative">
-            <div className="border-2 border-black p-8 rounded-md">
-              <div className="border-2 border-black p-6 rounded-md">
-                <div className="border-b-2 border-black">
-                  <h2 className="text-4xl font-bold text-black  text-center font-mono tracking-wider">
-                    Create an Account
-                  </h2>
-                </div>
-                <form
-                  onSubmit={handleSubmit}
-                  className="space-y-4 mt-6 font-mono"
-                >
-                  <div className="flex items-center space-x-4">
-                    <label className="text-lg w-28 text-black">Fullname</label>
-                    <input
-                      type="text"
-                      name="fullname"
-                      value={formData.fullname}
-                      onChange={handleChange}
-                      className="border border-black rounded px-3 py-1 w-full"
-                      required
-                    />
-                  </div>
-                  <div className="flex items-center space-x-4">
-                    <label className="text-lg w-28 text-black">Username</label>
-                    <input
-                      type="text"
-                      name="username"
-                      value={formData.username}
-                      onChange={handleChange}
-                      className="border border-black rounded px-3 py-1 w-full"
-                      required
-                    />
-                  </div>
-                  <div className="flex items-center space-x-4">
-                    <label className="text-lg w-28 text-black">Email</label>
-                    <input
-                      type="email"
-                      name="email"
-                      value={formData.email}
-                      onChange={handleChange}
-                      className="border border-black rounded px-3 py-1 w-full"
-                      required
-                    />
-                  </div>
-                  <button
-                    type="submit"
-                    className="bg-black text-white px-6 py-2 rounded-md w-full mt-4 text-lg tracking-wide"
-                  >
-                    Sign Up
-                  </button>
-                </form>
+      <div className="bg-[#f5f2e5] h-screen flex items-center justify-center">
+        <div className="border-2 border-black p-8 rounded-lg relative">
+          <div className="border-2 border-black p-8 rounded-md">
+            <div className="border-2 border-black p-6 w-max rounded-md">
+              <div className="border-b-2 border-black ">
+                <h2 className="text-6xl font-bold text-black text-center font-gravity tracking-wider">
+                  Login
+                </h2>
               </div>
+              <form
+                onSubmit={handleSubmit}
+                className="space-y-4 mt-6 font-mono"
+              >
+                <div className="flex items-center space-x-4">
+                  <label className="text-xl font-semibold w-28 text-black ">Username</label>
+                  <input
+                    type="text"
+                    name="username"
+                    value={formData.username}
+                    onChange={handleChange}
+                    className="border border-black rounded px-3 py-1 w-full"
+                    required
+                  />
+                </div>
+                <div className="flex items-center space-x-4">
+                  <label className="text-xl font-semibold  w-28 text-black">Passkey</label>
+                  <input
+                    type="password"
+                    name="privateKey"
+                    value={formData.privateKey}
+                    onChange={handleChange}
+                    className="border border-black rounded px-3 py-1 w-full"
+                    required
+                  />
+                </div>
+                <button
+                  type="submit"
+                  className="bg-black text-white px-6 py-2 rounded-md w-full mt-4 text-lg tracking-wide"
+                >
+                  Login
+                </button>
+              </form>
             </div>
           </div>
         </div>
-      {/* </GridLines> */}
+      </div>
     </>
   );
 };
 
-export default SignupPage;
+export default LoginPage;
