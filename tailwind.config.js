d6d46ad9ec8f04c51d1dd148d3adbe9c6eda11aa
Aditya-Bajpayee007 <adityabajpayee7@gmail.com>
2025-01-23 17:14:52 +0530

    tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,7 +8,19 @@ export default {
     extend: {
       fontFamily: {
         gravity: ['fs-gravity'], // Add custom font here
-      },
+      },
+      animation: {
+        flag: "flag 1s ease-in-out infinite",
+      },
+      keyframes: {
+        flag: {
+          //rotate the image
+          "0%, 100%": { transform: "rotate(0deg)" },
+          "25%": { transform: "rotate(1deg)" },
+          "50%": { transform: "rotate(-1deg)" },
+          "75%": { transform: "rotate(1deg)" },
+        },
+      },
     },
   },
   plugins: [],
