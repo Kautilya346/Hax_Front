328d28305952c1d08d162abbe2e5efc6e917e7df
Aditya Karanwal <23ucs514@lnmiit.ac.in>
2025-01-24 23:23:28 +0530

    23_23

--- src/pages/ExplorePage.jsx
+++ src/pages/ExplorePage.jsx
@@ -13,7 +13,8 @@ const ExplorePage = () => {
       dp: "https://randomuser.me/api/portraits/men/1.jpg",
       name: "John Doe",
       work: "I will redesign existing Wix, Wix Studio, and Squarespace website",
-      description:"I am a professional web designer with 5 years of experience. I have worked with many clients and have a good track record.",
+      description:
+        "I am a professional web designer with 5 years of experience. I have worked with many clients and have a good track record.",
       price: "500",
       mainphoto: "https://randomuser.me/api/portraits/men/1.jpg",
       contact: "1234567890",
@@ -22,7 +23,8 @@ const ExplorePage = () => {
       dp: "https://randomuser.me/api/portraits/women/2.jpg",
       name: "Aditya Bajpayee",
       work: "I will redesign existing Wix, Wix Studio, and Squarespace website",
-      description: "I am a professional web designer with 5 years of experience. I have worked with many clients and have a good track record.",
+      description:
+        "I am a professional web designer with 5 years of experience. I have worked with many clients and have a good track record.",
       price: "500",
       mainphoto: "https://randomuser.me/api/portraits/women/2.jpg",
       contact: "1234567890",
@@ -31,7 +33,8 @@ const ExplorePage = () => {
       dp: "https://randomuser.me/api/portraits/women/4.jpg",
       name: "Srivas",
       work: "I will redesign existing Wix, Wix Studio, and Squarespace website",
-      description: "I am a professional web designer with 5 years of experience. I have worked with many clients and have a good track record.",
+      description:
+        "I am a professional web designer with 5 years of experience. I have worked with many clients and have a good track record.",
       price: "500",
       mainphoto: "https://randomuser.me/api/portraits/women/4.jpg",
       contact: "1234567890",
@@ -40,7 +43,8 @@ const ExplorePage = () => {
       dp: "https://randomuser.me/api/portraits/women/4.jpg",
       name: "Srivas",
       work: "I will redesign existing Wix, Wix Studio, and Squarespace website",
-      description: "I am a professional web designer with 5 years of experience. I have worked with many clients and have a good track record.",
+      description:
+        "I am a professional web designer with 5 years of experience. I have worked with many clients and have a good track record.",
       price: "500",
       mainphoto: "https://randomuser.me/api/portraits/women/4.jpg",
       contact: "1234567890",
@@ -49,7 +53,8 @@ const ExplorePage = () => {
       dp: "https://randomuser.me/api/portraits/men/5.jpg",
       name: "Kumar Ji",
       work: "I will redesign existing Wix, Wix Studio, and Squarespace website",
-      description: "Hi, I'm Aditya, a passionate developer with expertise in the MERN stack. I specialize in creating dynamic, responsive websites with a focus on performance and user experience. I'm always eager to learn new technologies and love solving complex problems. Let's build something amazing together!",
+      description:
+        "Hi, I'm Aditya, a passionate developer with expertise in the MERN stack. I specialize in creating dynamic, responsive websites with a focus on performance and user experience. I'm always eager to learn new technologies and love solving complex problems. Let's build something amazing together!",
       price: "500",
       mainphoto: "https://randomuser.me/api/portraits/men/5.jpg",
       contact: "1234567890",
@@ -58,7 +63,8 @@ const ExplorePage = () => {
       dp: "https://randomuser.me/api/portraits/men/6.jpg",
       name: "Mandav Para",
       work: "I will redesign existing Wix, Wix Studio, and Squarespace website",
-      description: "I am a professional web designer with 5 years of experience. I have worked with many clients and have a good track record.",
+      description:
+        "I am a professional web designer with 5 years of experience. I have worked with many clients and have a good track record.",
       price: "500",
       mainphoto: "https://randomuser.me/api/portraits/men/6.jpg",
       contact: "1234567890",
@@ -67,7 +73,8 @@ const ExplorePage = () => {
       dp: "https://randomuser.me/api/portraits/men/7.jpg",
       name: "Kautila N",
       work: "I will redesign existing Wix, Wix Studio, and Squarespace website",
-      description: "I am a professional web developer with 5 years of experience in web development. I have worked with many clients and have a good track record.",
+      description:
+        "I am a professional web developer with 5 years of experience in web development. I have worked with many clients and have a good track record.",
       price: "500",
       mainphoto: "https://randomuser.me/api/portraits/men/7.jpg",
       contact: "1234567890",
@@ -76,7 +83,8 @@ const ExplorePage = () => {
       dp: "https://randomuser.me/api/portraits/men/8.jpg",
       name: "Nath Godam",
       work: "I will redesign existing Wix, Wix Studio, and Squarespace website",
-      description: "I am a professional web designer with 5 years of experience. I have worked with many clients and have a good track record.",
+      description:
+        "I am a professional web designer with 5 years of experience. I have worked with many clients and have a good track record.",
       price: "500",
       mainphoto: "https://randomuser.me/api/portraits/men/8.jpg",
       contact: "1234567890",
@@ -85,7 +93,8 @@ const ExplorePage = () => {
       dp: "https://randomuser.me/api/portraits/women/1.jpg",
       name: "Jai Jinendra",
       work: "I will redesign existing Wix, Wix Studio, and Squarespace website",
-      description: "I am a professional web designer with 5 years of experience. I have worked with many clients and have a good track record.",
+      description:
+        "I am a professional web designer with 5 years of experience. I have worked with many clients and have a good track record.",
       price: "500",
       mainphoto: "https://randomuser.me/api/portraits/women/1.jpg",
       contact: "1234567890",
@@ -98,7 +107,7 @@ const ExplorePage = () => {
 
   const handleClick = (person) => {
     navigate("/hire", { state: { person } });
-  }
+  };
 
   return (
     <div className="h-full w-screen">
@@ -129,17 +138,20 @@ const ExplorePage = () => {
               {activePerson && (
                 <div className="fixed top-0 left-0 w-full h-full flex items-center justify-center bg-black bg-opacity-50 z-10">
                   <div
-                    className="h-fit w-11/12 mx-auto absolute z-20 bg-[#efe9ca] bg-gradient-to-r from-[#bdaa6d] via-white to-[#bdaa6d] bg-opacity-85 p-6 rounded-lg shadow-2xl backdrop-blur-lg border border-gray-300 transition-all duration-500 ease-in-out transform opacity-0 scale-75"
+                    className="h-fit w-11/12 mx-auto absolute z-20 bg-[#f5f2e5] bg-opacity-85 p-6 rounded-lg shadow-2xl backdrop-blur-lg border border-gray-300 transition-all duration-500 ease-in-out transform opacity-0 scale-75"
                     style={{ animation: "fadeIn 0.5s forwards" }}
-                    onMouseEnter={() => setActivePerson(activePerson)} 
-                    onMouseLeave={() => setActivePerson(null)} 
+                    onMouseEnter={() => setActivePerson(activePerson)}
+                    onMouseLeave={() => setActivePerson(null)}
                   >
                     <img
                       src={activePerson.mainphoto}
                       alt="Main"
                       className="w-full h-72 object-contain rounded-lg"
                     />
-                    <button  onClick={() => handleClick(activePerson)} className="absolute top-4 right-4 bg-[#3b3314] text-white px-4 py-2 rounded-lg font-semibold">
+                    <button
+                      onClick={() => handleClick(activePerson)}
+                      className="absolute top-4 right-4 font-ibm-flex-mono bg-[#3b3314] text-white px-4 py-2 rounded-lg font-semibold"
+                    >
                       HIRE
                     </button>
                     <div className="mt-4 flex items-center gap-4">
@@ -149,23 +161,29 @@ const ExplorePage = () => {
                         className="w-16 h-16 object-cover rounded-full"
                       />
                       <div>
-                        <h3 className="text-2xl font-bold text-gray-800">
+                        <h3 className="text-2xl font-ibm-flex-mono font-bold text-gray-800">
                           {activePerson.name}
                         </h3>
-                        <p className="text-gray-600 text-sm mt-1">{activePerson.work}</p>
+                        <p className="text-gray-600 text-sm mt-1">
+                          {activePerson.work}
+                        </p>
                       </div>
                     </div>
                     <div className="mt-2 text-wrap">
-                      <span className="font-bold">Description : </span><span className="">{activePerson.description}</span>
+                      <span className="font-bold">Description : </span>
+                      <span className="">{activePerson.description}</span>
                     </div>
                     <div className="text-gray-900 text-lg font-semibold mt-3">
                       From ₹{activePerson.price}
                     </div>
                     <div className="mt-1">
-                      <span><span className="font-bold">Contact : </span>{activePerson.contact}</span>
+                      <span>
+                        <span className="font-bold">Contact : </span>
+                        {activePerson.contact}
+                      </span>
                     </div>
                   </div>
-                  </div>
+                </div>
               )}
             </div>
           </div>
