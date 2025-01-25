2dc09c560980c91d9a91b8a0b28774ff0bedea85
Aditya-Bajpayee007 <adityabajpayee7@gmail.com>
2025-01-26 04:07:04 +0530

    ka

--- src/components/CoinButton.jsx
+++ src/components/CoinButton.jsx
@@ -4,13 +4,13 @@ import axios from "axios";
 
 async function initiateTransaction(receiverAddress,amount,setShowCoin){
     try{
-    //   const resp=await axios.post(`http://localhost:3000/transaction/sendmoney`,{
-    //     receiverAddress,
-    //     amount
-    //   },{
-    //     withCredentials:true
-    //   });
-    //   console.log(resp.data);
+      const resp=await axios.post(`http://localhost:3000/transaction/sendmoney`,{
+        receiverAddress,
+        amount
+      },{
+        withCredentials:true
+      });
+      console.log(resp.data);
 
       setShowCoin(true);
     //   setTimeout(() => {
@@ -51,40 +51,38 @@ function CoinButton({balance,user1Bool,user2Bool,receiverAddress,amount}) {
             >
                 <div className="relative flex flex-col">
                     <motion.div
-                        className="w-80 h-80 bg-yellow-400 rounded-full border-4 border-yellow-500 shadow-lg relative bg-center"
-                        style={{
-                            backgroundImage:
-                            "url('https://res.cloudinary.com/dturzqo8m/image/upload/v1737806039/depositphotos_40481065-stock-photo-coin_iihs0f.webp')",
-                            backgroundSize: "fill",
-                        }}
-                        initial={{ scale: 0, rotate: 0 }}
-                        animate={{ scale: 1.5, rotate: 360 }}
-                        transition={{ duration: 0.8, ease: "easeOut" }}
+                        className="w-96 h-72 mt-12 bg-white rounded-xl border-4 border-[#DC483A] shadow-lg relative bg-center"
+                        initial={{ scale: 0 }}
+                        animate={{ scale: 1.5 }}
+                        transition={{ duration: 0.5, ease: "easeOut" }}
                         >
                         <div className="absolute inset-0 flex flex-col justify-center px-6 text-gray-800 text-lg font-bold font-mono">
-                        <div className="flex justify-between w-full">
-                            <span>Gas Money:</span>
-                            <span>APT 30,000</span>
-                        </div>
-                        <div className="flex justify-between w-full">
-                            <span>Transaction:</span>
-                            <span>APT 30,000</span>
-                        </div>
-                        <div className="flex justify-between w-full">
-                            <span>Current Balance:</span>
-                            <span>APT {balance}</span>
-                        </div>
+                            <div className="flex justify-between w-full">
+                                <span>Gas Money:</span>
+                                <span>APT {resp.userTransactionResponse.gas_used * resp.userTransactionResponse.gas_unit_price}</span>
+                            </div>
+                            <div className="flex justify-between w-full">
+                                <span>Transaction:</span>
+                                <span>APT {amount}</span>
+                            </div>
+                            <div className="flex justify-between w-full">
+                                <span>Current Balance:</span>
+                                <span>APT {resp.resource.coin.value}</span>
+                            </div>
+                            <button className="mt-6 w-fit mx-auto px-3 py-1 bg-[#ffcfcb] border-2 border-black text-xl  rounded-md hover:bg-[#DC483A] transition-all">
+                                Okay, got it!
+                            </button>
                         </div>
                     </motion.div>
                     <motion.p
-                        className="mt-24 text-center text-xl font-semibold text-green-600"
+                        className="relative text-center text-xl font-semibold text-green-600"
                         initial={{ opacity: 0, y: 20 }}
                         animate={{ opacity: 1, y: 0 }}
                         exit={{ opacity: 0, y: 20 }}
-                        transition={{ duration: 0.5 }}
-                    >
+                        transition={{ duration: 1 }}
+                        >
                         Transaction Completed!
-                </motion.p>
+                    </motion.p>
                 </div>
             </motion.div>
             )}
