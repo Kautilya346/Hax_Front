99e61f5287643d34976cedb36b55008a59cd7d7f
992manav <dmanavpianist@gmail.com>
2025-01-25 13:14:32 +0530

    changes by me

--- src/components/PeopleCard.jsx
+++ src/components/PeopleCard.jsx
@@ -1,12 +1,33 @@
 import React, { useState } from "react";
 
-export const PeopleCard = ({ dp, name, work, price, mainphoto,description,contact,domain, setActivePerson }) => {
+export const PeopleCard = ({
+  dp,
+  name,
+  work,
+  price,
+  mainphoto,
+  description,
+  contact,
+  domain,
+  setActivePerson,
+  userid,
+}) => {
   const [hoverTimeout, setHoverTimeout] = useState(null);
 
   const handleMouseEnter = () => {
     const timeout = setTimeout(() => {
-      setActivePerson({ dp, name, work, price, mainphoto,description,contact,domain });
-    }, 500); 
+      setActivePerson({
+        dp,
+        name,
+        work,
+        price,
+        mainphoto,
+        description,
+        contact,
+        domain,
+        userid,
+      });
+    }, 500);
     setHoverTimeout(timeout);
   };
 
@@ -28,7 +49,7 @@ export const PeopleCard = ({ dp, name, work, price, mainphoto,description,contac
           className="object-fill w-4/5 mx-auto py-5 "
         />
       </div>
-      <hr className="border-black border-2 mx-10 rounded-lg"/>
+      <hr className="border-black border-2 mx-10 rounded-lg" />
       <div className="p-4">
         <div className="flex gap-2 items-center">
           <img
@@ -38,9 +59,12 @@ export const PeopleCard = ({ dp, name, work, price, mainphoto,description,contac
           />
           <h3 className="text-l font-bold text-black">{name}</h3>
         </div>
-        <p className="text-black text-sm mt-1 font-semibold">{domain[0].toUpperCase() + domain.slice(1)}</p>
+
         <p className="text-black text-sm mt-2">{work}</p>
-        <p className="text-black text-base font-semibold mt-3">
+        <p className="text-black text-sm mt-1 font-semibold mt-3">
+          Domain:{domain[0].toUpperCase() + domain.slice(1)}
+        </p>
+        <p className="text-black text-base font-semibold ">
           Price : APT{price}
         </p>
       </div>
